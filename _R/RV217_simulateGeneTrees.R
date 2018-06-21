18180abaeb50d3c0b2594876bd4b0c820aea27a5
elewitus <elewitus@users.noreply.github.com>
2018-06-21 14:53:29 -0400

    Add files via upload

---
+++ _R/RV217_simulateGeneTrees.R
@@ -0,0 +1,142 @@
+library(RPANDA)
+library(phytools);PSIM_FAST <- TRUE
+library(phylosim)
+setwd('~/Desktop/_mtree/_simulated')
+load('~/Desktop/_mtree/RV217/AlignedByParticipant/_genes/_geneBeast/RV217bestMCCtrees_genes.RData')
+
+#gene lengths
+env=2646
+gag=1503
+nef=633
+pol=3013
+rev=351
+tat=306
+vif=585
+vpr=291
+vpu=246
+length<-c(rep(env,43),rep(gag,43),rep(nef,43),rep(pol,43),rep(rev,43),rep(tat,43),rep(vif,43),rep(vpr,43),rep(vpu,43))
+
+#variants
+options<-c('normal','discrete','invariant')
+basefreq<-list(c(0.4,0.3,0.2,0.1),c(0.1,0.2,0.3,0.4))
+alpha<-c(10,1)
+beta<-c(1,10)
+
+
+##JC69
+c()->jc69
+for(o in 1:length(options)){
+	print(o)
+jc69[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],model='JC69',option=options[o])	
+	})
+}	
+
+lapply(1:length(jc69),function(i){
+	lapply(1:length(jc69[[i]]),function(j){
+		saveAlignment(jc69[[i]][[j]],file=paste("JC69",options[i],j,"fas",sep=""),skip.internal=T)
+	})
+})
+
+##HKY
+#basefreq1_highAlpha_lowBeta
+c()->hky_basefreq1_highAlpha_lowBeta
+for(o in 1:length(options)){
+	print(o)
+hky_basefreq1_highAlpha_lowBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='HKY',alpha=alpha[1],beta=beta[1],basefreq=basefreq[[1]],option=options[o])	
+	})
+#basefreq2_highAlpha_lowBeta
+c()->hky_basefreq2_highAlpha_lowBeta
+for(o in 1:length(options)){
+	print(o)
+hky_basefreq2_highAlpha_lowBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='HKY',alpha=alpha[1],beta=beta[1],basefreq=basefreq[[2]],option=options[o])	
+	})
+#basefreq1_lowAlpha_highBeta
+c()->hky_basefreq1_lowAlpha_highBeta
+for(o in 1:length(options)){
+	print(o)
+hky_basefreq1_lowAlpha_highBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='HKY',alpha=alpha[2],beta=beta[2],basefreq=basefreq[[1]],option=options[o])	
+	})
+#basefreq2_lowAlpha_highBeta
+c()->hky_basefreq2_lowAlpha_highBeta
+for(o in 1:length(options)){
+	print(o)
+hky_basefreq2_lowAlpha_highBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='HKY',alpha=alpha[2],beta=beta[2],basefreq=basefreq[[2]],option=options[o])	
+	})
+
+
+##F81
+#basefreq1
+c()->f81_basefreq1
+for(o in 1:length(options)){
+	print(o)
+f81_basefreq1[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],model='F81',basefreq=basefreq[[1]],option=options[o])	
+	})
+#basefreq2
+c()->f81_basefreq2
+for(o in 1:length(options)){
+	print(o)
+f81_basefreq2[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],model='F81',basefreq=basefreq[[2]],option=options[o])	
+	})	
+	
+
+##K80
+#basefreq1_highAlpha_lowBeta
+c()->k80_basefreq1_highAlpha_lowBeta
+for(o in 1:length(options)){
+	print(o)
+k80_basefreq1_highAlpha_lowBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='k80',alpha=alpha[1],beta=beta[1],basefreq=basefreq[[1]],option=options[o])	
+	})
+#basefreq2_highAlpha_lowBeta
+c()->k80_basefreq2_highAlpha_lowBeta
+for(o in 1:length(options)){
+	print(o)
+k80_basefreq2_highAlpha_lowBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='k80',alpha=alpha[1],beta=beta[1],basefreq=basefreq[[2]],option=options[o])	
+	})
+#basefreq1_lowAlpha_highBeta
+c()->k80_basefreq1_lowAlpha_highBeta
+for(o in 1:length(options)){
+	print(o)
+k80_basefreq1_lowAlpha_highBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='k80',alpha=alpha[2],beta=beta[2],basefreq=basefreq[[1]],option=options[o])	
+	})
+#basefreq2_lowAlpha_highBeta
+c()->k80_basefreq2_lowAlpha_highBeta
+for(o in 1:length(options)){
+	print(o)
+k80_basefreq2_lowAlpha_highBeta[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],
+		model='k80',alpha=alpha[2],beta=beta[2],basefreq=basefreq[[2]],option=options[o])	
+	})	
+	
+##GTR
+#basefreq1
+c()->gtr_basefreq1
+for(o in 1:length(options)){
+	print(o)
+gtr_basefreq1[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],model='gtr',basefreq=basefreq[[1]],option=options[o])	
+	})
+#basefreq2
+c()->gtr_basefreq2
+for(o in 1:length(options)){
+	print(o)
+gtr_basefreq2[[o]]<-lapply(1:length(genetrees),function(l){
+		seqsim(genetrees[[l]],length=length[l],model='gtr',basefreq=basefreq[[2]],option=options[o])	
+	})	
+
---
+++ _R/seqsim.R
@@ -0,0 +1,57 @@
+seqsim<-function(tree,length,basefreq,alpha,beta,model=c(''),option=c(''),superinfect=F){
+	##JC69
+	##Equal substitution rates and equal base frequencies (Jukes and Cantor, 1969)
+	if(model=='JC69'){
+		mod<-JC69()
+	}
+	##HKY 
+	##Unequal transition/transversion rates and unequal base freq. (Hasegawa, Kishino and Yano, 1985)
+	if(model=='HKY'){
+		mod<-HKY(rate.params=list("Alpha"=alpha,"Beta"=beta), base.freqs=basefreq)
+			}
+	##F81
+	##Equal rates but unequal base freq. (Felsenstein, 1981)
+	if(model=='F81'){
+		mod<-list(F81(base.freqs=basefreq))
+			}
+	##K80
+	##Unequal transition/transversion rates and equal base freq. (Kimura, 1980)
+	if(model=='K80'){
+		mod<-K80(rate.params=list("Alpha"=alpha,"Beta"=beta), base.freqs=basefreq)
+			}
+	##GTR
+	##General time reversible model with unequal rates and unequal base freq. (Tavare, 1986)
+	if(model=='GTR'){
+		mod<-GTR(rate.params=list('a'=1,'b'=2,'c'=3,'d'=1,'e'=2,'f'=3),base.freqs=basefreq)
+			}
+	if(option=='discrete'){
+		sim<-PhyloSim(sampleStates(plusGamma(
+					NucleotideSequence(len=length,proc=list(list(mod))),
+					mod,0.5)),phylo=tree)
+			}
+	if(option=='invariant'){
+		sim<-PhyloSim(sampleStates(plusInvGamma(
+					NucleotideSequence(len=length,proc=list(list(mod))),
+					mod,pinv=0.8,0.5)),phylo=tree)
+			}
+	if(option=='normal'){
+		sim<-PhyloSim(sampleStates(NucleotideSequence(len=length,proc=list(list(mod)))),
+			phylo=tree)
+			}
+	if(superinfect==T){
+			super<-function(seq){
+				if(!isAttached(seq$sites[[1]],mod)){
+					return(seq);
+				}
+				cat("Resampling rate multipliers");
+				plusGamma(seq,mod,1)
+				return(seq)
+				}
+			attachHookToNode(sim,node=length(tree$tip.label)+1,fun=super)
+		alignment<-Simulate(sim)
+			}	
+	if(superinfect==F){
+		alignment<-Simulate(sim)
+			}
+	return(alignment)	
+} 	
