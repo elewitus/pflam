8895b5064bf1a90e265de247c2f3563abfccbd4e
elewitus <elewitus@users.noreply.github.com>
2018-08-16 11:49:48 -0400

    Add files via upload

--- _R/modeltest.R
+++ _R/modeltest.R
@@ -3,29 +3,25 @@ library(phytools)
 library(phylosim);PSIM_FAST <- TRUE
 setwd('~/Desktop/_mtree/_simulated')
 source('seqsim.R')
-load('RV217bestMCCtrees_genes.RData')
+rcoal(30)->tre
 
 #MODELTEST
 k80<-lapply(1:10,function(l){
   print(l)
   seqsim(tre,length=20,model='K80',alpha=10,beta=1,basefreq=c(0.4,0.3,0.2,0.1),option='normal')
+  saveAlignment(t,file=paste("k80",l,".fas",sep=""),skip.internal=T)
 })
 
 hky<-lapply(1:10,function(l){
   print(l)
   seqsim(tre,length=20,model='HKY',alpha=10,beta=1,basefreq=c(0.4,0.3,0.2,0.1),option='normal')
+  saveAlignment(t,file=paste("hky",l,".fas",sep=""),skip.internal=T)
+	rm(t)
 })
 
 gtr<-lapply(1:10,function(l){
   print(l)
   seqsim(tre,length=20,model='GTR',basefreq=c(0.4,0.3,0.2,0.1),option='normal')
+  saveAlignment(t,file=paste("gtr",l,".fas",sep=""),skip.internal=T)
 })
 
-
-mods<-c(k80,hky,gtr)
-mod<-c(rep("k80",10),rep("hky",10),rep("gtr",10))
-
-
-lapply(1:length(mods),function(i){
-  saveAlignment(mods[[i]],file=paste(mod[i],i,".fas",sep=""),skip.internal=T)
-})		
