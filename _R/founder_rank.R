00e0738b2b7dca29c5b10d91a47a3df4428dbd55
elewitus <elewitus@users.noreply.github.com>
2019-06-02 09:39:34 -0400

    add jump, partition methods

--- _R/founder_rank.R
+++ _R/founder_rank.R
@@ -1,7 +1,8 @@
 library(igraph)
 library(ape)
+library(fpc)
 
-founder_rank<-function(phylos,sd=F,plot=F){
+founder_rank<-function(phylos,sd=F,test=c('median','jump','partition'),g=0,plot=F){
 	
 #gaussian kernel
 sigma = 0.1
@@ -79,13 +80,14 @@ integr <- function(x, f)
 		}
 		
 	#define function for finding heterogeneity rank for each tree
-	founder_test<-function(phylos,test){
+	median_founder_test<-function(phylos,test){
 	
 	#compute median+-0.5*sigma^2
+	g=g
 	med<-function(x){
-		c(median(log(x))-0.5*sd(log(x)),median(log(x))+0.5*sd(log(x)),median(log(x)))}
+		c(median(log(x))-g*sd(log(x)),median(log(x))+g*sd(log(x)),median(log(x)))}
 	
-	#draw the threshold at +-0.5*sd or at the median
+	#draw the threshold at +-g*sd or at the median
 	if(sd==T){
 		if(log(pe[[test]])<med(pe)[1]){return('homogeneous')}	
 		if(log(pe[[test]])>med(pe)[2]){return('heterogeneous')}	
@@ -97,24 +99,73 @@ integr <- function(x, f)
 		}
 	}
 	
+	jump_founder_test<-function(pe){
+		sortedpe<-sort(log(pe))[-c(1,length(pe))]
+		which.max(diff(sortedpe))->threshold
+		sapply(1:length(pe),function(o){
+			if(log(pe[[o]])<pe[[threshold]]){return('homogeneous')}
+			if(log(pe[[o]])>pe[[threshold]]){return('heterogeneous')}
+		})
+	}
+	
+	clus_founder_test<-function(pe){
+		pamk(pe,2)->clus
+		sapply(1:length(clus[[1]]$partitioning),function(h){
+			if(clus[[1]]$partitioning[h]==1){return('homogeneous')}
+			if(clus[[1]]$partitioning[h]==2){return('heterogeneous')}
+		})
+	}
+	
 	#create table of founder ranks for all trees in set
+	if(test=='median'){
 	ranks<-sapply(1:length(phylos),function(r){
-		founder_test(phylos,r)
+		median_founder_test(phylos,r)
 	})
+	}
+	
+	if(test=='jump'){
+		ranks<-jump_founder_test(pe)
+	}
+	
+	if(test=='partition'){
+		ranks<-clus_founder_test(pe)
+	}
+	
 	#return trees by PE rank and founder classification
 	tab_pe<-as.data.frame(cbind(1:length(phylos),pe))
 	tab<-as.data.frame(cbind(as.numeric(order(tab_pe[,2],tab_pe[,1])),ranks[order(tab_pe[,2],tab_pe[,1])]))
 	colnames(tab)<-c('tree','founder_type')
 	
 	#plot PE (shifted by min) in order of tree input, demarcating +- 1/2 standard deviations from the median
-	if(plot==T){
+	if(plot==T & test=='median'){
 		min(log(pe))->shift
 		barplot(sort((log(pe)-shift)),col=colors(1)[runif(1,30,502)],
-			xlab='phylogeny',ylab=expression(lambda ~'* (shifted)'),
+			xlab='phylogeny',ylab=expression('ln'~lambda ~'* (shifted)'),
 			names=tab$rank,cex.names=0.75)
-		abline(h=median(log(pe))-0.5*sd(log(pe))-shift,lty=3)
-		abline(h=median(log(pe))+0.5*sd(log(pe))-shift,lty=2)	
+		abline(h=median(log(pe))-g*sd(log(pe))-shift,lty=3)
+		abline(h=median(log(pe))+g*sd(log(pe))-shift,lty=2)	
+		abline(h=median(log(pe)),lty=1)
 		return(tab)
+	}
+	if(plot==T & test=='jump'){
+		min(log(pe))->shift
+		barplot(sort((log(pe)-shift)),col=colors(1)[runif(1,30,502)],
+			xlab='phylogeny',ylab=expression('ln'~lambda ~'* (shifted)'),
+			names=tab$rank,cex.names=0.75)
+			sortedpe<-sort(log(pe))[-c(1,length(pe))]
+			sortedpe[which.max(diff(sortedpe))]->threshold
+		abline(h=threshold-shift,lty=3)
+		return(tab)
+	}
+	if(plot==T & test=='partition'){
+		min(log(pe))->shift
+		barplot(sort(log(pe)-shift),col=colors(1)[runif(1,30,502)],
+			xlab='phylogeny',ylab=expression('ln'~lambda ~'* (shifted)'),
+			names=tab$rank,cex.names=0.75)
+			pamk(sort(pe),2)->clus
+			sort(log(pe))[which(clus[[1]]$partitioning==2)[1]-1]->threshold
+			abline(h=threshold-shift,lty=2)
+		return(list(tab,clus[[1]]$clusinfo))
 	}
 	else{return(tab)}
 }	
